c1b6a8cd6290b3368536cda1de41d885f85757ba
haganjam <james_hagan@outlook.com>
2025-05-08 16:54:39 +0200

    add a function that allows the user to load the equation database

---
+++ R/view_equation_database.R
@@ -0,0 +1,21 @@
+#' @title view_equation_database()
+#' @description Get the equation database as a tibble()
+#' @details This function loads the equation database as a tibble() so that
+#' users can examine and filter if if they want to find relevant equations.
+#' @author James G. Hagan (james_hagan(at)outlook.com)
+#' @return tibble with chosen traits or equations based on the input parameters
+#' @export
+#' @importFrom dplyr
+view_equation_database <- function() {
+
+  # load the trait database
+  if (!exists(paste0(trait, "_db"))) {
+    assign(
+      "equation_db",
+      readRDS(file = get_db_file_path(paste0("equation", "_database.rds")))
+    )
+  }
+
+  return(dplyr::tibble(equation_db))
+
+}
