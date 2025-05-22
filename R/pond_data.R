f8732c45f84bc8f48f9f66209bd7d84673be305f
haganjam <james_hagan@outlook.com>
2025-05-22 14:27:39 +0200

    add an example dataset directly into the package

---
+++ R/pond_data.R
@@ -0,0 +1,22 @@
+
+#' Invertebrates collected in temporary ponds
+#'
+#'The test data consist of invertebrate taxa collected in six temporary ponds
+#'in a nature reserve in Belgium. In addition, the data consist of
+#'latitude-longitude coordinates for the samples as well as measured body
+#'lengths for all the taxa.
+#'
+#' @docType data
+#'
+#' @usage data(ponds)
+#'
+#' @format An object of class data.frame
+#'
+#' @keywords datasets
+#'
+#' @references Vincent Dolmans (Vincent.Dolmans@vub.be)
+#'
+#' @examples
+#' data(ponds)
+#'
+"ponds"
